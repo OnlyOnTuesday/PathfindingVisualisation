848710dee0c9c068ccb8a63c8e0a92e05b26d2cb
Michael Cooney <MC509119@ohio.edu>
2021-02-20 15:54:57 -0500

    created block class outline to represent the walls and path of the maze

---
+++ block.h
@@ -0,0 +1,31 @@
+#ifndef MAZE_H
+#define MAZE_H
+#include colors.h
+
+/*
+ * different colors
+ * each block should be a square that is 1 space
+ * 
+ * Needs to: placeBlock, 
+ */
+
+class Block {
+ public:
+
+  Block(){
+  }
+  std::void placeBlock(int x, int y);
+
+  //getters
+  std::char getType();
+  std::int getSize();
+
+  //setters
+  std::void setType(char type);
+  std::void setSize(int size);
+
+ private:
+
+}
+
+#endif
